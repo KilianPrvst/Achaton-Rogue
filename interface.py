342dd418d6953177999f83fa723f62e9bf599bbd
pierreberranger <pierre@berranger.eu>
2022-01-28 11:43:39 +0100

    e

--- interface.py
+++ interface.py
@@ -7,15 +7,16 @@ class Niveau:
     pass   
 class Salle:
 
-    def __init__(self, x0, y0, longueur, hauteur, portes):
+    def __init__(self, x0, y0, longueur, hauteur, portes, attributs):
         self.x0 = x0
         self.y0 = y0
         self.longueur = longueur
         self.hauteur = hauteur
         self.portes = portes
+        self.attributs = attributs
     
     def dans_salle(self, x, y):
-        return (x<self.x0 + self.longueur ) and (x>self.x0) and (y<self.y0 + self.hauteur) and (y>self.y0 )
+        return (x<self.x0 + self.longueur ) and (x>=self.x0) and (y<self.y0 + self.hauteur ) and (y>=self.y0 )
     
 
     def dans_porte(self, x, y):
@@ -23,9 +24,9 @@ class Salle:
     
 
     
-    def salle(self, screen):
-        rect1 = pg.Rect(self.x0, self.y0, 20*self.longueur, 20*self.hauteur)
-        rect2 = pg.Rect(self.x0, self.y0, 20*(self.longueur -1), 20*(self.hauteur -1))
+    def affiche(self, screen):
+        rect1 = pg.Rect(taille*(self.x0 -1), taille(self.y0-1), taille*self.longueur, taille*self.hauteur)
+        rect2 = pg.Rect(self.x0*taille, self.y0*taille, taille*(self.longueur +2), taille*(self.hauteur +2))
         pg.draw.rect(screen, (255,0,0), rect1 )
         pg.draw.rect(screen, (0,255,0), rect2 )
 
@@ -34,3 +35,4 @@ class Salle:
 
 salle = Salle(0,0,10,10,[])
 print(salle.dans_salle(9,9))
+
