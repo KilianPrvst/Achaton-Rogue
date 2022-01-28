43d7812bcf348eb0f51c9c49c8fbbc001706398f
pierreberranger <pierre@berranger.eu>
2022-01-28 10:30:11 +0100

    e

--- interface.py
+++ interface.py
@@ -1,2 +1,15 @@
 import pygame as pg
+from random import randint
 
+class Niveau:
+    pass   
+class Salle:
+
+    def __init__(self, x0, y0, longueur, hauteur):
+        self.x0 = x0
+        self.y0 = y0
+        self.longueur = longueur
+        self.hauteur = hauteur
+    
+    def dans_salle(self, x, y):
+        if x<
