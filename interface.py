2e2949b64b87b30aa5429114f0d1f0a80393bef8
pierreberranger <pierre@berranger.eu>
2022-01-28 11:29:32 +0100

    e

--- interface.py
+++ interface.py
@@ -2,7 +2,7 @@ import pygame as pg
 from random import randint
 import random  as rd
 import numpy as np
-
+taille =20
 class Niveau:
     pass   
 class Salle:
@@ -15,7 +15,7 @@ class Salle:
         self.portes = portes
     
     def dans_salle(self, x, y):
-        return (x<self.x0 + self.longueur) and (x>self.x0) and (y<self.y0 + self.hauteur) and (y>self.y0)
+        return (x<self.x0 + self.longueur ) and (x>self.x0) and (y<self.y0 + self.hauteur) and (y>self.y0 )
     
 
     def dans_porte(self, x, y):
@@ -24,12 +24,13 @@ class Salle:
 
     
     def salle(self, screen):
-        rect1 = pg.Rect(self.x0, self.y0, self.longueur, self.hauteur)
-        rect2 = pg.Rect(self.x0, self.y0, self.longueur -20, self.hauteur - 20)
+        rect1 = pg.Rect(self.x0, self.y0, 20*self.longueur, 20*self.hauteur)
+        rect2 = pg.Rect(self.x0, self.y0, 20*(self.longueur -1), 20*(self.hauteur -1))
         pg.draw.rect(screen, (255,0,0), rect1 )
         pg.draw.rect(screen, (0,255,0), rect2 )
 
 
     
 
-
+salle = Salle(0,0,10,10,[])
+print(salle.dans_salle(9,9))
